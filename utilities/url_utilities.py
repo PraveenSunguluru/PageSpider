f2da06ce5ba755d5e73483b48dbd176220a701ef
Praveen Sunguluru <praveen.sunguluru236@gmail.com>
2021-11-23 23:03:50 +0530

    Initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(filepath:str):
+    # TODO : add the code needed to read the text file with the URLS's in it
+    pass
+
+def load_page(url:str):
+    # TODO : add the code that reads the url
+    pass
+
+def scrape_page(page_contents:str):
+    # TODO : analyse the text
+    pass
+
